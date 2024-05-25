e0fd8f1a7e12746902640bd8dc48808ecfec2f7b
David Samaniego <119967932+DavidSamaniego95@users.noreply.github.com>
2024-05-24 22:42:58 -0700

    stop countdown

--- script.js
+++ script.js
@@ -7,10 +7,18 @@ let countDown = new Date('may 25, 2024 17:30:00').getTime(),
     x = setInterval(function() {
       let now = new Date().getTime(),
           distance = countDown - now;
+          if (distance > 0 ){
         document.getElementById('dias').innerText = Math.floor(distance / (day)),
         document.getElementById('horas').innerText = Math.floor((distance % (day)) / (hour)),
         document.getElementById('minutos').innerText = Math.floor((distance % (hour)) / (minute)),
         document.getElementById('segundos').innerText = Math.floor((distance % (minute)) / second);
+          } else {
+              document.getElementById('dias').innerText = 00,
+              document.getElementById('horas').innerText = 00,
+              document.getElementById('minutos').innerText = 00,
+              document.getElementById('segundos').innerText = 00;
+          }
+          
     }, second);
 /*screen.orientation.onchange = function () {
   console.log(screen.orientation.type + " W: " +screen.width + " H: " +screen.height);
